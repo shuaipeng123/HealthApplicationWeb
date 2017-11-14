bd8dd99391cb260cf9679d6d01a515e567e174a9
shuaipeng123 <s36peng@uwaterloo.ca>
2017-11-13 22:20:36 -0500

    login_page_setup

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
 const functions = require('firebase-functions');
+const express=require('express');
+const engines=require('consolidate');
 
+const app=express();
+app.engine('ejs',engines.ejs);
+app.set('views','./views');
+app.set('view engine','ejs');
+app.get('/timestamp',(request,response)=>{
+ response.render('login.ejs');
+});
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from hh!");
-});
+exports.app = functions.https.onRequest(app);
